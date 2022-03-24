8c6bfe60caaeb9e89680cb98223acb6ee669d12c
leonchik.r <leonchik.roman01@gmail.com>
2022-03-24 10:43:45 +0300

    Add CDN

--- next.config.js
+++ next.config.js
@@ -10,3 +10,10 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   i18n,
 };
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/Roman-Leonchik/portfolio/gh-pages/' : '',
+}
