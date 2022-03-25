f2aa2485c0385d5a8e1435302d462fc569abe278
leonchik.r <leonchik.roman01@gmail.com>
2022-03-25 10:31:04 +0300

    Change module

--- next.config.js
+++ next.config.js
@@ -5,14 +5,14 @@ const nextConfig = {
 
 module.exports = nextConfig
 
-const { i18n } = require('./next-i18next.config');
-
-module.exports = {
-  i18n,
-};
-
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   assetPrefix: isProd ? '/portfolio/' : ''
 }
+
+const { i18n } = require('./next-i18next.config');
+
+module.exports = {
+  i18n,
+};
